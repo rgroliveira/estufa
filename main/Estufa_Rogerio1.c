7d2d43f3457517f23b11dc7f871beb90812e5732
rgroliveira <rgroliveira@yahoo.com.br>
2025-09-03 20:11:28 -0300

    modified:   main/Estufa_Rogerio1.c 	new file:   main/Pinos_Franzininho_LAB01.h

--- main/Estufa_Rogerio1.c
+++ main/Estufa_Rogerio1.c
@@ -40,29 +40,7 @@ static const char *TAG_VERSAO = "Estufa r1.01";
 #include "dht.h"      // Biblioteca DHT para DHT11
 #include "ssd1306.h"  // Biblioteca SSD1306 para OLED
 #include "RMemoria_NVS.h" 
-
-
-//Pinos
-#define PINO_LDR                1   //Pino do LDR, GPIO 34 é um pino apenas de entrada
-#define PINO_BOTAO_ESC   2   //LAB01 GPIO2
-#define PINO_BOTAO_OK   3   //LAB01 GPIO3
-#define PINO_BOTAO_BAIXO        4   //LAB01 GPIO4
-#define PINO_BOTAO_DIREITA      5   //LAB01 GPIO5
-#define PINO_BOTAO_ESQUERDA     6   //LAB01 GPIO6
-#define PINO_BOTAO_CIMA         7   //LAB01 GPIO7
-
-#define PINO_I2C_SDA            8   //LAB01 SDA      
-#define PINO_I2C_SCL            9   //LAB01 SCL     
-#define PINO_OLED_RESET         -1  // Nao implementado no LAB01, GPIO -1    
-
-#define PINO_LED_B              12   //LAB01 LED Azul
-#define PINO_LED_G              13   //LAB01 LED Verde
-#define PINO_LED_R              14   //LAB01 LED Vermelho
-#define PINO_RELE               14   //Pino do relé, GPIO 14 ( no LAB01 é o LED vermelho
-#define PINO_DHT11              15
-#define PINO_BUZZER             17   //LAB01 Buzzer
-#define PINO_LED_F1             21   //LED Franzininho
-#define PINO_LED_F2             33   //LED Franzininho
+#include "Pinos_Franzininho_LAB01.h"
 
 //DHT11 configuration
 static const dht_sensor_type_t DHT11_TIPO = DHT_TYPE_DHT11;
